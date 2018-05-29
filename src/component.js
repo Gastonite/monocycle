012b2f483570532d1092175904381bc988dacd32
Clément Pasquier <clement@n00sphere.net>
2018-05-29 18:48:55 +0200

    add code

---
+++ src/component.js
@@ -0,0 +1,41 @@
+import isFunction from 'lodash.isfunction'
+
+const makeComponent = ({
+  methods = {}
+} = {}) => {
+
+  const Component = factory => {
+
+    if (!isFunction(factory))
+      factory = Component.empty
+
+    if (factory.isComponent)
+      return factory
+
+    const map = f => Component(
+      Object.assign(f(factory), methods)
+    )
+
+    return Object.assign(factory,
+      (Object.keys(methods) || []).reduce((before, key) => {
+
+        const method = methods[key]
+
+        return !isFunction(method)
+          ? before
+          : Object.assign({}, before, {
+            [key]: method(map)
+          })
+
+      }, {}), {
+        isComponent: true,
+        map,
+      })
+  }
+
+  Component.empty = () => ({})
+
+  return Component
+}
+
+export default makeComponent
