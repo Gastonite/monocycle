023cbe8edc21d483db5187a5678e632b90844434
Clément Pasquier <clement@n00sphere.net>
2018-11-22 13:52:07 +0100

    spread steps to ramda pipe

--- utilities/pipe.js
+++ utilities/pipe.js
@@ -1,6 +1,6 @@
 const RamdaPipe = require('ramda/src/pipe')
 
-const pipe = (firstStep, ...steps) => (firstArg = void 0, ...args) => RamdaPipe(steps)(firstStep(firstArg, ...args))
+const pipe = (firstStep, ...steps) => (firstArg = void 0, ...args) => RamdaPipe(...steps)(firstStep(firstArg, ...args))
 
 
 module.exports = {
