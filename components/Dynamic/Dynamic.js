ed11acce56f0607185c0ba861db9c3a1dc1c0bbb
Clément Pasquier <clement@n00sphere>
2018-11-22 20:04:22 +0100

    Dynamic: update invalid paths

--- components/Dynamic/Dynamic.js
+++ components/Dynamic/Dynamic.js
@@ -2,17 +2,18 @@ const $ = require('xstream').default
 const isFunction = require('ramda-adjunct/lib/isFunction').default
 const unless = require('ramda/src/unless')
 const always = require('ramda/src/always')
-const { ensurePlainObj } = require('monocycle/utilities/ensurePlainObj')
+const { pipe } = require('../../utilities/pipe')
+const { ensurePlainObj } = require('../../utilities/ensurePlainObj')
 const lensProp = require('ramda/src/lensProp')
 const over = require('ramda/src/over')
 const defaultTo = require('ramda/src/defaultTo')
 const when = require('ramda/src/when')
-const pipe = require('ramda/src/pipe')
-const { isComponent } = require('monocycle/component')
+// const pipe = require('ramda/src/pipe')
+const { isComponent } = require('../../component')
 const objOf = require('ramda/src/objOf')
 const applyTo = require('ramda/src/applyTo')
 const { extractSinks } = require('cyclejs-utils')
-const { makeEmptyObject } = require('monocycle/utilities/empty')
+const { makeEmptyObject } = require('../../utilities/empty')
 
 const WithDynamic = pipe(
   when(isFunction, objOf('from')),
