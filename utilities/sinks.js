3720c72691b28de7db4da84451e7e1bf9aa391cf
Clément Pasquier <clement@n00sphere>
2018-12-19 21:27:45 +0100

    sinks: refactoring

--- utilities/sinks.js
+++ utilities/sinks.js
@@ -2,9 +2,11 @@ const { Stream: $ } = require('xstream')
 const pipe = require('ramda/src/pipe')
 const uniq = require('ramda/src/uniq')
 const __ = require('ramda/src/__')
-const has = require('ramda/src/has')
+const reject = require('ramda/src/reject')
+const contains = require('ramda/src/contains')
 const filter = require('ramda/src/filter')
 const map = require('ramda/src/map')
+const has = require('ramda/src/has')
 const concat = require('ramda/src/concat')
 const reduce = require('ramda/src/reduce')
 
@@ -14,8 +16,9 @@ const reduce = require('ramda/src/reduce')
  * @param 'sinks' the sinks to be merged
  * @param 'exceptions' a dictionary of special channels, e.g. DOM
  * @return the new unified sinks 
- */
-const mergeSinks = (allSinks, exceptions = {}) => {
+ * 
+ * 
+ * {
 
   const emptySinks = pipe(
     reduce(pipe(
@@ -50,6 +53,45 @@ const mergeSinks = (allSinks, exceptions = {}) => {
         .map(key => ({ [key]: exceptions[key](combinedSinks[key]) }))
     ),
     reduce(Object.assign, {})
+  )(combinedSinks)
+}
+ */
+const mergeSinks = (allSinks, exceptions = {}) => {
+
+  const emptySinks = pipe(
+    reduce((before, sinks) => before.concat(Object.keys(sinks)), []),
+    uniq,
+    map(key => ({ [key]: [] })),
+    reduce(Object.assign, {})
+  )(allSinks)
+
+  const combinedSinks = allSinks.reduce((before, curr) => {
+    return pipe(
+      Object.keys,
+      map(key => ({ [key]: !curr[key] ? before[key] : [...before[key], curr[key]] })),
+      reduce(Object.assign, {})
+    )(before)
+  }, emptySinks)
+
+  return pipe(
+
+    //merge
+    Object.keys,
+    reject(contains(__, Object.keys(exceptions))),
+    map(key => ({
+      [key]: combinedSinks[key].length === 1 ? combinedSinks[key][0] : $.merge(...combinedSinks[key])
+    })),
+
+    //add specials
+    concat(__,
+      pipe(
+        Object.keys,
+        filter(has(__, combinedSinks)),
+        map(key => ({ [key]: exceptions[key](combinedSinks[key]) })),
+      )(exceptions)
+    ),
+
+    reduce(Object.assign, {}),
   )(combinedSinks)
 }
 
